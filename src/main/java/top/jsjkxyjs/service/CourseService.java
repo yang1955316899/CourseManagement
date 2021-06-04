7b7d6301f3212b5551e3eccf4e5febf443138c83
yang1955316899 <1955316899@qq.com>
2021-06-04 10:40:41 +0800

    杨建文      这两个文件有毒,一直提交不上去

---
+++ src/main/java/top/jsjkxyjs/service/CourseService.java
@@ -0,0 +1,7 @@
+package top.jsjkxyjs.service;
+
+import top.jsjkxyjs.entity.Course;
+
+public interface CourseService {
+	boolean setCourse(Course course);
+}
---
+++ src/main/java/top/jsjkxyjs/service/impl/CourseServiceImpl.java
@@ -0,0 +1,12 @@
+package top.jsjkxyjs.service.impl;
+
+import top.jsjkxyjs.dao.impl.CourseDaoImpl;
+import top.jsjkxyjs.entity.Course;
+import top.jsjkxyjs.service.CourseService;
+
+public class CourseServiceImpl implements CourseService {
+	@Override
+	public boolean setCourse(Course course) {
+		return new CourseDaoImpl().setCourse(course);
+	}
+}
